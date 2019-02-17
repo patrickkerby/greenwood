e25baa2876dbf9d777cfba86ac333c32fff63c9e
Patrick Kerby <patrick@kerbydesign.com>
2019-02-17 12:28:24 -0700

    Mobile Menu

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,11 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+    $('.hamburger').click(function() {
+      $(this).toggleClass('is-active');
+      $('.nav-mobile').toggleClass('is-active');
+      $('body').toggleClass('is-active');
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,20 +1,31 @@
-<?php
-
+@php
   $image = get_field('bg_image');
   $size = 'full'; // (thumbnail, medium, large, full or custom size)
   $overlay = get_field('overlay');	
   $sub_heading = get_field('sub_heading');
   
   $intro_bar = get_field('intro_bar');
+@endphp
 
-  if( $image ) { ?>
+ {{-- Hamburger Menu --}}
+ <button class="hamburger hamburger--minus d-md-none" type="button">
+    <span class="hamburger-box">
+      <span class="hamburger-inner"></span>
+    </span>
+  </button>
+  <nav class="nav-mobile">
+    @if (has_nav_menu('primary_navigation'))
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+    @endif
+  </nav>
+@php
+  if( $image ) { @endphp
       <header class="banner row angle--bottom-left" style="background-image: linear-gradient(rgba(34,39,32,0.<?php echo $overlay; ?>), rgba(34,39,32,0.<?php echo $overlay; ?>)), url('<?php echo $image; ?>');">
     <?php }
     else { ?>
       <header class="banner row angle--bottom-left">
-  <?php }
-  ?>
-    <nav class="nav-primary">
+  <?php } ?>
+    <nav class="nav-primary d-none d-md-block">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
