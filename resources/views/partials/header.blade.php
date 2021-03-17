dbaef257b507c5141b68de976d570ea1be0bb013
Patrick Kerby <patrick@kerbydesign.com>
2021-03-17 13:32:38 -0600

    Add mailchimp signup

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -30,9 +30,15 @@
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
     </nav>
-    <div class="subheading col-sm-8">
+    <div class="subheading col-sm-6">
       <img src="@asset('images/GreenwoodLogo.svg')" alt="Greenwood Distillers Logo" />
       <?php echo $sub_heading; ?>
+      <div class="row justify-content-center">
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+          Subscribe for updates!
+        </button>                     
+      </div>
     </div>
   </header>
   @if ( $intro_bar )
@@ -45,3 +51,53 @@
     </div>
   </section>
   @endif
+
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title" id="exampleModalLabel">Hi!</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body container">
+          <div class="row justify-content-center">
+            <p class="col-sm-7">We won't spam you. Promise. You can expect to recieve the occasional email about new products as we come up with them, updates to our taproom hours, etc.</p>
+          </div>
+          <div class="row justify-content-center">
+            <!-- Begin Mailchimp Signup Form -->
+            <div id="mc_embed_signup" class="col-sm-10 col-md-10">
+              <form action="https://greenwooddistillers.us7.list-manage.com/subscribe/post?u=aa10af12443a1e28c18e3f984&amp;id=3b6b5a64da" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+                  <div class="mc-field-group">                  
+                    <input type="email" value="" name="EMAIL" placeholder="Email Address *" class="required email" id="mce-EMAIL"> <br />
+                  </div>
+                  <div class="mc-field-group">
+                    <input type="text" value="" name="FNAME" class="" placeholder="First Name" id="mce-FNAME"> <br />
+                  </div>
+                  <div class="mc-field-group">
+                    <input type="text" value="" name="LNAME" class="" placeholder="Last Name" id="mce-LNAME"> <br />
+                  </div>
+                </div>
+                <div id="mce-responses" class="clear">
+                  <div class="response" id="mce-error-response" style="display:none"></div>
+                  <div class="response" id="mce-success-response" style="display:none"></div>
+                </div>    
+                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                  <input type="text" name="b_aa10af12443a1e28c18e3f984_3b6b5a64da" tabindex="-1" value="">
+                </div>
+                <div class="clear">
+                  <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+                </div>
+              </div>
+              </form>
+            </div>
+            <!--End mc_embed_signup-->
+          </div>
+        </div>        
+      </div>
+    </div>
+  </div> 
