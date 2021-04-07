4b16f8c1023cd790a8636942c3805a95d1fc2673
Patrick Kerby <patrick@kerbydesign.com>
2021-04-06 20:12:54 -0600

    fix borders for products

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
   @while(have_posts()) @php the_post() @endphp
     @include('partials.content-page')
 
-    <div class="row products">
+    <div class="row products justify-content-center">
        
         @if( have_rows('products') )     
           
@@ -18,7 +18,7 @@
             @if ($loop->count === 3 )
               <div class="col-sm-4 justify-content-center product">
             @else
-              <div class="col-sm-6 justify-content-center product">
+              <div class="col-sm-5 justify-content-center product">
             @endif
               <img class="thumb" src="{{ $item->product_thumbnail }}" alt="{{ $item->product_title }}" />
               <h2>{{ $item->product_title }}</h2>
@@ -27,62 +27,64 @@
               </div>
             {{-- Product Modal --}}
             @if ($item->product_details)
-            <div class="product_detail modal fade" id="exampleModalCenter-{{ $loop->iteration }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">                 
-              <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div class="modal-body">
-                    <div class="container-fluid">
-                      <div class="row justify-content-center">                            
-                        <div class="col-sm-8">{!! $item->product_details !!}</div>
-                      </div>
-                      {{-- Product Recipes --}}
-                      @if($item->recipes_title)
-                        <section id="recipes" class="stockists container">
-                          <div class="row justify-content-center">
-                            <div class="col-md-10">
-                              <h2>{{ $item->recipes_title }}</h2>
-                              <h3>{{ $item->recipes_subtitle }}</h3>
-                            </div>
-                          </div>
-                          <div class="row"> 
-                          @if($item->recipes)         
-                            @foreach($item->recipes as $recipe)
-                              <div class="col-md-4 col-sm-6 info">
-                                {!! $recipe->info !!}
-                                <svg viewBox="0 0 10 10"><polygon points="5,0 0,5 5,10 10,5"></polygon></svg>
+            <section>
+              <div class="product_detail modal fade" id="exampleModalCenter-{{ $loop->iteration }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">                 
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      <div class="container-fluid">
+                        <div class="row justify-content-center">                            
+                          <div class="col-sm-8">{!! $item->product_details !!}</div>
+                        </div>
+                        {{-- Product Recipes --}}
+                        @if($item->recipes_title)
+                          <section id="recipes" class="stockists container">
+                            <div class="row justify-content-center">
+                              <div class="col-md-10">
+                                <h2>{{ $item->recipes_title }}</h2>
+                                <h3>{{ $item->recipes_subtitle }}</h3>
                               </div>
-                            @endforeach
-                          @endif
-                          </div>
-                        </section>
-                        @else
-                      @endif
-                      {{-- Product specific gallery --}}
-                      
-                      @if($item->product_gallery)
-                      <section class="gallery row">
-                          <div class="grid-layout col-md-12">                            
-                              @foreach ($item->product_gallery as $photo)
-                                <div class="grid-item image" style="background-image: url('{{ $photo->sizes->large }}');">
-                                  <a href="{{ $photo->url }}" target="_blank"></a>
+                            </div>
+                            <div class="row"> 
+                            @if($item->recipes)         
+                              @foreach($item->recipes as $recipe)
+                                <div class="col-md-4 col-sm-6 info">
+                                  {!! $recipe->info !!}
+                                  <svg viewBox="0 0 10 10"><polygon points="5,0 0,5 5,10 10,5"></polygon></svg>
                                 </div>
                               @endforeach
-                          </div>  
-                        </section>
-                       @endif 
-                      <div class="row justify-content-center">
-                        <img src="@asset('images/GreenwoodBadge.svg')" alt="Greenwood Distillers Logo" class="logo-badge col-sm-4" />
+                            @endif
+                            </div>
+                          </section>
+                          @else
+                        @endif
+                        {{-- Product specific gallery --}}
+                        
+                        @if($item->product_gallery)
+                        <section class="gallery row">
+                            <div class="grid-layout col-md-12">                            
+                                @foreach ($item->product_gallery as $photo)
+                                  <div class="grid-item image" style="background-image: url('{{ $photo->sizes->large }}');">
+                                    <a href="{{ $photo->url }}" target="_blank"></a>
+                                  </div>
+                                @endforeach
+                            </div>  
+                          </section>
+                        @endif 
+                        <div class="row justify-content-center">
+                          <img src="@asset('images/GreenwoodBadge.svg')" alt="Greenwood Distillers Logo" class="logo-badge col-sm-4" />
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </section>
             @else
             @endif
           @endforeach
